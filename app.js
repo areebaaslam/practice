9e5fbd2dccf55a9d75fc482be27555eda309a168
Areeba Aslam <110449436+areebaaslam@users.noreply.github.com>
2022-09-02 10:26:47 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,125 @@
+                    //  chapter no 1 alert
+
+
+              //  1.  alert ("greate your website");  
+              //  2.  document.write("display"); 
+              //  3. document.write("welcome to js land <br> happy landing"); 
+                  4.  //  Hide() or fadeout(), then the next()  
+              //  5.   console.log("helloworld"); 
+                  6.
+              //  7.    body(after your page html)
+                          
+                //   
+                
+                
+                // chapter no 2    verible or string
+
+                
+                
+                
+                //  1.  var username ;
+                    2.    // var myName = "areebaaslam"; 
+                    // 3.    b.
+            //  4. var a = "biodata";
+            //   var b = "student";
+            //   alert(a + b );
+            // 5.  var thnx = "thanks for your input";
+            //  alert(thnx);
+            // 6. var emailName = "My email address is";
+            // var emailId  = "example@example.com";
+            // alert(emailName + "" + emailId );
+            // 7.  var book = "A smatter way to learn javascript";
+            //   alert(book);
+            // 8.   var display = "yah! i can write html content through java script";
+            //    document.write("display");
+            //9. var myText = "They can be whatever text you like";
+            //  document.write("They can be whatever text you like");
+
+
+
+            
+            
+            // chapter 3 'verible of Number'
+            //1. var age = "15"
+            // alert("age");
+            // 2. var i = 0
+            // for (var i=0;i<10;i++){
+            //     var j = i%2;
+            // }
+            //  3 .var birthyear = "1997";
+            // document.write("my birth year is 1990");
+            // 4.  a. visitor's name
+
+            // 5. var name = "john deo ordered"
+            //  var name2 = "5 t-shirt on xyz clothing store"
+            //  document.write("john deo ordered  5 t-shirt on xyz clothing store  ");
+
+
+            // chapter 4 'legal or ilegal'
+
+            // var verible1 = "helloworld";
+            // var varible2 = "testing...";
+            // var verible3 =  58; 
+            
+            
+            // var abc (legal)
+            // var ABC (legal)
+            // var ab1 (legal)
+            // var $abc(legal)
+            // var ab1.$c  (legal)
+            // var 123 (inlegal)
+            // var 1ab (inlegal)
+            // abc8c   (inlegal)
+
+
+                // chapter 5  'Math Experssions'
+            //1. var popularrnum = 3+5;
+            // document.write(3+5);
+
+            //2. var popularnum = 3-5;
+            // document.write(3-5);
+            //    var popularnum = 3*5;
+            //    document.write(3*5);
+            //  var num = 5%3;
+            //  document.write(5%3);
+
+            // 3. var popularnum = 4;
+            //  4 way to declare verible
+            // using var
+            // using let
+            // using const
+            // using nothing
+            //  type veriblename;
+            //  veriblename = experssion;
+
+            // var num = 5;
+            // document.write(num);
+            // var a = 5;
+            // var b = ++a;
+            // document.write(b);
+            // var a = 6;
+            // var b = ++a;
+            // document.write(b);
+            //  var x = 12;
+            //  var y = 1;
+            //  var z = x + y;
+            //  document.write(z);
+            //  var num = 13;
+            //  var newnum = --num;
+            //  document.write(newnum);
+            //  var num = 10%5;
+            //  document.write(num);
+
+            // 4. var costofmovie = 600;
+            // var ticketprice = 5;
+            // var totalcosttobuyticketmovieis = 600*5;
+            // document.write(totalcosttobuyticketmovieis);
+
+            //5. var no = 4;
+            // for(i=1; i<=10; i++){
+            //     console.log(no + "*" +i+ "=" +no*i);
+            // }
+
+            
+            
+            
